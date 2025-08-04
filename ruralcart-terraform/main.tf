97fb0a850366be0789bbb1bf52021a993a77e462
Jithinrmklu <jithinrmklu@gmail.com>
2025-08-04 23:28:26 +0530

    Kubenetes

---
+++ ruralcart-terraform/main.tf
@@ -0,0 +1,60 @@
+provider "kubernetes" {
+    config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "ruralcart" {
+  metadata {
+    name = "ruralcart"
+    labels = {
+      app = "ruralcart"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "ruralcart"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "ruralcart"
+        }
+      }
+
+      spec {
+        container {
+          name  = "ruralcart"
+          image = "ruralcart:dev"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "ruralcart" {
+  metadata {
+    name = "ruralcart"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.ruralcart.metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+  
+}
